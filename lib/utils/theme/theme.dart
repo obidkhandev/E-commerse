a1ae14604977a636fb960cd635f1a121d5290908
@alwaysflutter <obidkhandev@gmail.com>
2024-02-14 19:23:05 +0500

    text theme, ligth mode and dark mode

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,23 @@
+import 'package:e_commerse/utils/theme/custom_themes/text_theme.dart';
+import 'package:flutter/material.dart';
+
+class TAppTheme {
+  TAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.light,
+    fontFamily: "Poppins",
+    scaffoldBackgroundColor: Colors.white,
+    primaryColor: Colors.blue,
+    textTheme: TTextTheme.lightTextTheme,
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.dark,
+    fontFamily: "Poppins",
+    scaffoldBackgroundColor: Colors.black,
+    primaryColor: Colors.blue,
+    textTheme: TTextTheme.darkTextTheme,
+  );
+}
