fe5941c2fb98a5833f09283be25a1c5782828529
@alwaysflutter <obidkhandev@gmail.com>
2024-02-16 23:44:22 +0500

    pricing calculator

---
+++ lib/utils/helpers/pricing_calculator.dart
@@ -0,0 +1,33 @@
+class TPricingCalculator {
+  // --Calculate Price based on tax and shipping
+  static double calculateTotalPrice(double productPrice, String location) {
+    double taxRate = getTaxRateForLocation(location);
+    double taxAmount = productPrice * taxRate;
+
+    double shipppingCost = getShippingCost(location);
+
+    double totalPrice = productPrice + taxAmount + shipppingCost;
+    return totalPrice;
+  }
+
+  // --Calculate shipping cost
+  static String calculateShippingCost(double productPrice, String location) {
+    double shippingCost = getShippingCost(location);
+    return shippingCost.toStringAsFixed(2);
+  }
+
+  // --Calculate tax
+  static String calculateTax(double productPrice, String location) {
+    double taxRate = getTaxRateForLocation(location);
+    double taxAmount = productPrice * taxRate;
+    return taxAmount.toStringAsFixed(2);
+  }
+
+  static double getTaxRateForLocation(String location) {
+    return 0.10;
+  }
+
+  static double getShippingCost(String location) {
+    return 5.00;
+  }
+}
